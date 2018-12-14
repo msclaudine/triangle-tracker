1f0c435fdf542f57f9329e9b6e6ea34de03f3ec3
musaclo@gmail.com <musaclo@gmail.com>
2018-12-14 15:19:42 +0200

    calculation of sum of tiangle sides that will be used in testing and its transformation into integer

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-function triangleCheck() {
+ function triangleCheck() {
   var A = parseInt(document.trianleSides.A.value);
   var B = parseInt(document.trianleSides.B.value);
   var C = parseInt(document.trianleSides.C.value);
+
+  var AB = parseInt(document.trianleSides.A.value) + parseInt(document.trianleSides.B.value);
+  var AC = parseInt(document.trianleSides.A.value) + parseInt(document.trianleSides.C.value);
+  var BC = parseInt(document.trianleSides.B.value) + parseInt(document.trianleSides.C.value);
+
+  var txt;
