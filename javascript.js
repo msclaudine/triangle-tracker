c646937168cfc9514b2fd3209665684f1d780103
musaclo@gmail.com <musaclo@gmail.com>
2018-12-14 15:16:03 +0200

    creation of function and variable declaration and their transformation in interger

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function triangleCheck() {
+  var A = parseInt(document.trianleSides.A.value);
+  var B = parseInt(document.trianleSides.B.value);
+  var C = parseInt(document.trianleSides.C.value);
