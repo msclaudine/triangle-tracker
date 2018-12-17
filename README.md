990f77df7b6cfab3303bdb41bad00698a77f1c0c
musaclo@gmail.com <musaclo@gmail.com>
2018-12-17 11:11:03 +0200

    identing code and adding contents into read me file

--- README.md
+++ README.md
@@ -4,14 +4,12 @@ This program (TRIANGLE TRACKER PROGRAM)is developed by MUSANABANDI CLAUDINE.
 
 It helps in tracking the type of triangle by intering the three sides of triangle.
 
-
 SETUP
 If you would like a local copy of this website, make sure you have git installed then run:
 
-$ git clone https://github.com/msclaudine/triangle-tracker.git
+\$ git clone https://github.com/msclaudine/triangle-tracker.git
 
 you may browse it from:
-
 
 Technologies used:
 HTML
@@ -23,17 +21,21 @@ JAVASCRIPT
 Triangle tracking
 
 1. define invalid input
-  eg:sides that have lenght less than or equals to zero (A<=0 or B<=0 0r c<=0 or both sides equal to zero or negative values).
-  if this conditioin is not true
+   eg:sides that have lenght less than or equals to zero (A<=0 or B<=0 0r c<=0 or both sides equal to zero or negative values).
+   if this conditioin is not true
 
 2. check if triangle is equilateral by cheking if all its sides are equal
-    
-      eg:12,12,12.
-     
-    if this condition also is also not true
+
+   eg:12,12,12.
+
+   if this condition also is also not true
 
 3. it checks if triangle is isosceles by checking if it has any two sides which are equal
-      eg:25,25,9.
-      also when it found this condition not satisified
+   eg:25,25,9.
+   also when it found this condition not satisified
 
-4. it checks if inputed sides form a scalene triangle by checking 
+4. it checks if inputed sides form a scalene triangle by checking to see
+   if all sides are different (no side equal to another)and the sum of any two sides is greater than the rest one
+   eg:3,4,9
+5. after check if given sides can form a triangle by checking if the sum of any two sides are not less than or equal to the rest one
+   when the sum of any two sides is less than or equal to the third one; those can note form a triangle
--- javascript.js
+++ javascript.js
@@ -1,34 +1,41 @@
- function triangleCheck() {
+function triangleCheck() {
   var A = parseInt(document.trianleSides.A.value);
   var B = parseInt(document.trianleSides.B.value);
   var C = parseInt(document.trianleSides.C.value);
 
-  var AB = parseInt(document.trianleSides.A.value) + parseInt(document.trianleSides.B.value);
-  var AC = parseInt(document.trianleSides.A.value) + parseInt(document.trianleSides.C.value);
-  var BC = parseInt(document.trianleSides.B.value) + parseInt(document.trianleSides.C.value);
-  
-  var txts=["INVALID INPUTS","THIS IS EQUILATERAL TRIANGLE","THIS IS ISOSCELENES TRIANGLE","THIS IS SCALENE TRIANGLE",
-  "CAN NOT FORM A TRIANGLE", "THIS IS NOT VALID NUMBERS"];
+  var AB =
+    parseInt(document.trianleSides.A.value) +
+    parseInt(document.trianleSides.B.value);
+  var AC =
+    parseInt(document.trianleSides.A.value) +
+    parseInt(document.trianleSides.C.value);
+  var BC =
+    parseInt(document.trianleSides.B.value) +
+    parseInt(document.trianleSides.C.value);
+
+  var txts = [
+    "INVALID INPUTS",
+    "THIS IS EQUILATERAL TRIANGLE",
+    "THIS IS ISOSCELENES TRIANGLE",
+    "THIS IS SCALENE TRIANGLE",
+    "CAN NOT FORM A TRIANGLE",
+    "THIS IS NOT VALID NUMBERS"
+  ];
   var out;
 
-  if ((A<=0) ||(B<= 0) ||(C<= 0) || (A==="") && (B==="") && (C==="")) {
-  return document.getElementById("me").innerHTML=txts[0]}
-  if ((A === C) && (A=== B) && (B===C)){
-      out=txts[1];
-     }
-     else if ((A===B) ||(A===C) || (B===C)) {
-      out=txts[2];
-      
-      }
-      else if ((AB > C) && (AC > B) && (BC > A) && (A!=B) && (A!=C) && (B!=C)) {
-        out=txts[3];}
-         
-         else if ((AB <=C) ||(AC<= B) ||(BC <= A) ) {
-          out=txts[4];}
-  
-        else {
-        out=txts[5];
-        }
-        document.getElementById("me").innerHTML=out   
-      }
-      
+  if (A <= 0 || B <= 0 || C <= 0 || (A === "" && B === "" && C === "")) {
+    return (document.getElementById("me").innerHTML = txts[0]);
+  }
+  if (A === C && A === B && B === C) {
+    out = txts[1];
+  } else if (A === B || A === C || B === C) {
+    out = txts[2];
+  } else if (AB > C && AC > B && BC > A && A != B && A != C && B != C) {
+    out = txts[3];
+  } else if (AB <= C || AC <= B || BC <= A) {
+    out = txts[4];
+  } else {
+    out = txts[5];
+  }
+  document.getElementById("me").innerHTML = out;
+}
